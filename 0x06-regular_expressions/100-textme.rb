c2cd1d666aa79fe30f387fb9c44366c279f22e44
Haytham El-Gebaly <hsmgebaly@gmail.com>
2023-08-03 06:54:20 -0700

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?) \] \[flags:(.*?)\]/).join(",")
+# This script examines log entries and gathers particular data.
+
+# each line of the input line by line
+ARGF.each do |line|
+  # Using regular expressions, extract sender, receiver, and flags
+  if line =~ /from:([^]]+).*to:([^]]+).*flags:([^]]+)/
+    sender = $1
+    receiver = $2
+    flags = $3
+    # format the information you output.
+    puts "#{sender},#{receiver},#{flags}"
+  end
+end
